5663dc7c0f0268df1e95beeead0f96594bd08c31
Daniel Zen <daniel@zendigital.com>
2012-06-18 14:58:40 -0400

    Added $event & greedy

--- app/js/jquery-ui-ng.js
+++ app/js/jquery-ui-ng.js
@@ -1,9 +1,7 @@
-/* http://docs.angularjs.org/#!angular.widget */
-
-(function(){
+(function () {
   function evalFn(element, scope, exp, property){
     property = property || '$token';
-    return function(token){
+    return function (token) {
       var old = scope.hasOwnProperty(property) ? scope[property] : undefined;
       scope[property] = token;
       var retVal = scope.$eval(exp, element);
@@ -15,42 +13,45 @@
   angular.module("jqui", [])
     .directive('jquiDragStart', function ($compile) {
       return {
-        link:function (scope, item, attrs) {
-          var dragStartExp = attrs.jquiDragStart || '';
-          var dragEndExp = attrs.jquiDragEnd || '';
-          var helperExp = attrs.jquiHelper || 'original';
-          var handle = attrs.jquiHandle || false;
-          var axisExp = attrs.jquiAxis;
+        link: function (scope, item, attrs) {
+          var dragStartExp = attrs.jquiDragStart || '',
+            dragEndExp = attrs.jquiDragEnd || '',
+            helperExp = attrs.jquiDragHelper || 'original',
+            handle = attrs.jquiHandle || false,
+            axisExp = attrs.jquiAxis,
+            dragStart = evalFn(item, scope, dragStartExp, '$event'),
+            dragEnd = evalFn(item, scope, dragEndExp),
+            helper = helperExp,
+            token;
 
           item.addClass('jqui-dnd-item');
 
-          var dragStart = evalFn(item, scope, dragStartExp);
-          var dragEnd = evalFn(item, scope, dragEndExp);
-          var helper = helperExp;
-          if (!(helper == 'original' || helper == 'clone')) {
+          if (!(helper === 'original' || helper === 'clone')) {
             helper = evalFn(item, scope, helperExp);
           }
-          var token;
 
           item.draggable({
-            addClass:false,
+            addClass: false,
             handle: handle,
             helper: helper,
-            start:function (event, ui) {
-              item.draggable('option', 'revertDuration', 200);
+            start: function (event, ui) {
+              item.draggable({revertDuration: 200,
+                distance: 5,
+                delay: 100});
               item.addClass('jqui-dnd-item-dragging');
-              item.data('jqui-dnd-item-token', token = dragStart());
+              item.data('jqui-dnd-item-token', token = dragStart(event));
               scope.$apply();
             },
-            stop:function () {
+            stop: function () {
               item.removeClass('jqui-dnd-item-dragging');
               item.removeClass('jqui-dnd-item-over');
               item.removeData('jqui-dnd-item-token');
+              item.removeData('jqui-dnd-event');
               dragEnd(token);
               scope.$apply();
               token = null;
             },
-            revert:true
+            revert: true
           });
 
           if (axisExp) {
@@ -59,23 +60,25 @@
             });
           }
         }
-      }
+      };
     })
 
     .directive('jquiDropCommit', function ($compile) {
       return {
-        link:function (scope, target, attrs) {
-          var acceptExp = attrs.jquiDropAccept || 'true';
-          var commitExp = attrs.jquiDropCommit || '';
+        link: function (scope, target, attrs) {
+          var acceptExp = attrs.jquiDropAccept || 'true',
+            commitExp = attrs.jquiDropCommit || '',
+            greedyExp = attrs.jquiDropGreedy || false,
+            accept = evalFn(target, scope, acceptExp),
+            commit = evalFn(target, scope, commitExp);
 
           target.addClass('jqui-dnd-target');
-          var accept = evalFn(target, scope, acceptExp);
-          var commit = evalFn(target, scope, commitExp);
 
           target.droppable({
-            addClass:false,
-            tolerance:'pointer',
-            activate:function (event, ui) {
+            greedy: greedyExp,
+            addClass: false,
+            tolerance: 'pointer',
+            activate: function (event, ui) {
               var token = ui.draggable.data('jqui-dnd-item-token');
               if (accept(token)) {
                 target.addClass('jqui-dnd-target-active');
@@ -84,26 +87,26 @@
               }
               scope.$apply();
             },
-            deactivate:function () {
+            deactivate: function () {
               target.removeClass('jqui-dnd-target-active');
               target.removeClass('jqui-dnd-target-disable');
               target.removeClass('jqui-dnd-target-over');
             },
-            over:function (event, ui) {
+            over: function (event, ui) {
               if (target.hasClass('jqui-dnd-target-active')) {
                 target.addClass('jqui-dnd-target-over');
                 ui.draggable.addClass('jqui-dnd-item-over');
               }
             },
-            out:function (event, ui) {
+            out: function (event, ui) {
               target.removeClass('jqui-dnd-target-over');
               ui.draggable.removeClass('jqui-dnd-item-over');
             },
-            drop:function (event, ui) {
+            drop: function (event, ui) {
               if (target.hasClass('jqui-dnd-target-active')) {
                 commit(ui.draggable.data('jqui-dnd-item-token'));
                 ui.draggable.draggable('option', 'revertDuration', 0);
-                ui.draggable.css({top:'', left:''});
+                ui.draggable.css({top: '', left: ''});
                 ui.draggable.draggable('option', 'stop')();
               }
               target.removeClass('jqui-dnd-target-active');
@@ -112,8 +115,7 @@
             }
           });
         }
-      }
+      };
     });
 
-})();
-
+}());
